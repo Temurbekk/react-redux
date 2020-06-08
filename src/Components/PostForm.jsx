bd1a31264423b59cc45697a749e5953d2ab65b2a
Temurbek Sabirov <prodota150@gmail.com>
2020-06-08 14:50:00 -0400

    Implement post

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -8,22 +8,49 @@ class PostForm extends Component {
       title: "",
       body: "",
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onChange = (event) => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  onSubmit = (event) => {
+    event.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body,
+    };
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", post)
+      .then((res) => console.log(res.data));
+  };
   render() {
     return (
       <div>
         <h1>Add post</h1>
-        <form action="">
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label>
             <br />
-            <input type="text" name="title" value={this.state.title} />
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              onChange={this.onChange}
+            />
           </div>
           <br />
           <div>
             <label>Body: </label>
             <br />
-            <textarea name="body" value={this.state.body} />
+            <textarea
+              name="body"
+              value={this.state.body}
+              onChange={this.onChange}
+            />
           </div>
           <br />
           <button type="submit"> Submit</button>
