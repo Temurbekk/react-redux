383d35cf58cd5236d65503b76ec514460670097a
Temurbek Sabirov <prodota150@gmail.com>
2020-06-08 14:11:24 -0400

    Add axios

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Posts extends Component {
+  componentWillMount() {
+    console.log("Ran");
+  }
   render() {
     return <div>Posts</div>;
   }
