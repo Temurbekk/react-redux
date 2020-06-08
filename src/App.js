5c8f8014851710e656d4a07743b10c8472247f87
Temurbek Sabirov <prodota150@gmail.com>
2020-06-08 14:59:51 -0400

    Create basic redux store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import Posts from "./Components/Posts";
 import PostForm from "./Components/PostForm";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware);
 
 function App() {
   return (
-    <div>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
